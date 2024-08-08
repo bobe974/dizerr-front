3141e103965bc06c4b1e9a673d2d8c47431763b1
etienneDev <etienne.baillif97436@gmail.com>
2024-08-08 17:00:20 +0200

    btn play

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavigationComponent } from "./layout/navigation/navigation.component";
+import {FaIconLibrary, FontAwesomeModule} from "@fortawesome/angular-fontawesome";  
+import { fontAwesomeIcons } from './shared/font-awesome-icons';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
   title = 'dizzerr-front';
+  private faIconLibrary = inject(FaIconLibrary);
+
+  ngOnInit(): void {
+    this.initFontAwesome();
+  }
+  private initFontAwesome() {
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
+
 }
---
+++ src/app/shared/font-awesome-icons.ts
@@ -0,0 +1,5 @@
+
+import {IconDefinition} from "@fortawesome/free-regular-svg-icons"
+import {faCirclePlay} from "@fortawesome/free-solid-svg-icons"
+
+export const fontAwesomeIcons: IconDefinition[] = [faCirclePlay]
